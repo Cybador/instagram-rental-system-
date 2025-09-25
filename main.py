76f6af2da8ce295298cdb7663601fdde0db4514b
Salvador Sevilla <cybador@gmail.com>
2025-09-25 12:55:33 -0600

    feat: Actualizar main.py y requirements.txt

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
 
 # --- Configuración de la Base de Datos ---
-DATABASE_URL = "sqlite:///rental.db"
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = os.getenv("DATABASE_URL") # Se leerá de las variables de entorno
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
